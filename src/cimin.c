74d7ae55c9b5599dc4bdad75ca38104de08b30c2
young170 <seongbin10209@gmail.com>
2023-04-02 01:23:23 +0900

    Add comments to work on delta debugging [04/02/23] - sbkim

--- src/cimin.c
+++ src/cimin.c
@@ -11,39 +11,88 @@ void end_program() {
 }
 
 void handle_signal(int sig) {
+	// if alarm signal, end program
 	if (sig == SIGALRM) {
-		printf("3 sec..\n");
+		fprintf(stderr, "Execution time passed 3 sec..\n");
 		exit(1);
-	} else if (sig == SIGINT) {
-        printf("quit..\n");
+	}
+	// if interrupt signal, clean up & end program
+	else if (sig == SIGINT) {
+        fprintf(stderr, "Program quitting due to interrupt..\n");
         end_program();
         exit(1);
     }
 }
 
 char * minimize_input(char * input, char * condition) {
-	char * test_input = input;
-	int input_length = strlen(test_input);
-	int sub_length = input_length - 1;
+	char * test_input = input;				// variable input
+	int input_length = strlen(test_input);	// length of input
+	int sub_length = input_length - 1;		// length of substring of input
+
+	// pipe init
+	int pipe_fd[2];
+	if (pipe(pipe_fd) == -1) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
 
 	while (sub_length > 0) {
 		for (int i = 0; i < input_length - sub_length - 1) {
 			char * head, tail;
 
-			strncpy(head, test_input, i);
-			int offset = i + sub_length + 1;
-			strncpy(tail, test_input + offset, input_length - 1);
+			strncpy(head, test_input, i);	// [0..i-1]
+			int offset = i + sub_length;	// [i+s..
+			strncpy(tail, test_input + offset, input_length);	// ..|t|-1]
 
 			char * output;
 			// char * output = program_exec(strcat(head, tail));
-			if (strcmp(output, condition) == 0) {
+
+			/*
+			/////////////////////////////////////////////////////
+			use fork() and execl() to get output
+			/////////////////////////////////////////////////////
+			int pid = fork;
+			if (pid < 0) {
+				fprintf(stderr, "fork failed..\n");
+				exit(1);
+			} else if (pid == 0) {
+				// child process executes binary file
+				// use pipe() to redirect stdin input
+				execl(exec_file, ???, NULL);
+				fprintf(stderr, "child process failed..\n");
+				exit(1);
+			} else {
+				// parent process waits for child to finish
+				waitpid(pid, &status, 0);	// need time also as global var
+
+				/////////////////////////////////////////////////////
+				recieve the output of child via stderr using unnamed pipe
+				/////////////////////////////////////////////////////
+				close(pipe_fd[1]);
+				char buf[BUFSIZ];
+				char * output
+				ssize_t num_bytes_read;
+				while ((num_bytes_read = read(pipe_fd[0], buf, BUFSIZ)) > 0) {
+					// Output the received data
+					write(output, buf, num_bytes_read);
+				}
+				close(pipe_fd[0]);
+				/////////////////////////////////////////////////////
+
+				timer.it_value.tv_sec = 0;
+				setitimer(ITIMER_REAL, &timer, NULL);
+			}
+			*/
+
+			// if the output of stderr contains the error keyword condition
+			if (strstr(output, condition) != NULL) {
 				return minimize_input(strcat(head, tail));
 			}
 		}
 
 		for (int i = 0; i < input_length - sub_length - 1) {
 			char * mid;
-			strncpy(mid, test_input + i, i + sub_length);
+			strncpy(mid, test_input + i, i + sub_length);	// [i..i+s-1]
 
 			char * output;
 			// char * output = program_exec(mid);
@@ -52,6 +101,7 @@ char * minimize_input(char * input, char * condition) {
 			}
 		}
 
+		// decrement substring by 1
 		sub_length = sub_length - 1;
 	}
 
@@ -68,9 +118,12 @@ int main(int argc, char *argv[]) {
 
     struct itimerval timer;
 
+	// flags of input options
+	// 0 : false, 1 : true
 	int iflag = 0;
 	int mflag = 0;
 	int oflag = 0;
+
 	int opt;
 
 	//pipe file descriptor, 0 for reading, 1 for writing
@@ -119,7 +172,7 @@ int main(int argc, char *argv[]) {
 				return 1;
 		}
 	}
-	memcpy(exec_file, argv[7], 16);		// ./a.out : target program
+	memcpy(exec_file, argv[7], 16);	// ./a.out : target program
 
 	//create pipe
 	if(pipe(pipeFD)!=0){
@@ -127,6 +180,9 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
+	/////////////////////////////////////////////////////
+	// move fork(), execl(), and pipe to minimize_input()
+	/////////////////////////////////////////////////////
     pid = fork();
     if (pid < 0) {
         fprintf(stderr, "fork failed..\n");
@@ -145,6 +201,7 @@ int main(int argc, char *argv[]) {
         timer.it_value.tv_sec = 0;
         setitimer(ITIMER_REAL, &timer, NULL);
     }
+	/////////////////////////////////////////////////////
 
     return 0;
 }
